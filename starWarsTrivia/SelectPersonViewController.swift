d3d5f0457320a413ba833a31698b55b3603995c5
Paul Defilippi <allinfo.pd@gmail.com>
2019-01-19 18:02:12 -0500

    created initial project and added some custom fonts

--- starWarsTrivia/SelectPersonViewController.swift
+++ starWarsTrivia/SelectPersonViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SelectPersonViewController: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stackBg.layer.cornerRadius = 10
     }
 
 
